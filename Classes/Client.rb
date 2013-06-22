96b38669350a9e7c25979f0214f9b1b2e57aac8b
Mike Mayer <mike@industrygraph.com>
2013-06-21 23:18:54 -0400

    add mike's files

---
+++ Classes/Client.rb
@@ -0,0 +1,7 @@
+class Client
+  attr_accessor :id, :name
+    def initialize (id, name)
+      @id = id
+      @name = name
+    end
+end
---
+++ Classes/Trade.rb
@@ -0,0 +1,6 @@
+class Trade
+  attr_accessor :id :portfolio :stock :shares :pps :-1/1 :value
+
+
+
+  (Stocks added to the portfolio and value deducted from cash position. Cannot have negative cash balance.)"
