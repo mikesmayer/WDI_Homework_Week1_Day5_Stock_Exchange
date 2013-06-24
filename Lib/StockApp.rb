7e6239a1c6da27f84507d8a9ef0c2e712310ced5
Mike Mayer <mayerms@gmail.com>
2013-06-24 15:38:47 -0400

    first commit from inside of RubyMine

--- Lib/StockApp.rb
+++ Lib/StockApp.rb
@@ -5,14 +5,14 @@ require_relative 'Portfolio'
 require_relative 'Trades'
 require_relative 'Stock_Price'
 
-puts "Welcome to WDI Stock Trading Terminal"
+puts 'Welcome to WDI Stock Trading Terminal'
 
-  puts "Please type the number correspnding to the menu item you would like to selection: "
-  puts "1 - See a list of all clients"
-  puts "2 - Add a new client"
-  puts "3 - Create a new portfolio and starter balance"
+  puts 'Please type the number correspnding to the menu item you would like to selection: '
+  puts '1 - See a list of all clients'
+  puts '2 - Add a new client'
+  puts '3 - Create a new portfolio and starter balance'
 
-  manager_menu_selection = gets.chomp.to_i
+  manager_menu_selection = gets.chomp
 
 case manager_menu_selection
  when 1
@@ -21,6 +21,7 @@ case manager_menu_selection
   Stock_Ledger.add_client
  when 3
   Stock_Ledger.add_portfolio
- end manager_menu_selection
+  else
+    puts 'Thanks for coming—come again!'
 end
 
