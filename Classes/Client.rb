230d3dd533849135c95688bdc9a5466a2377202d
Mike <Mike@i7-MacBook.local>
2013-06-24 08:35:23 -0400

    created Mayer branch and added client data back to client.rb file

--- Classes/Client.rb
+++ Classes/Client.rb
@@ -36,4 +36,16 @@ class Client
   end
 end
 
-## MOVED CLIENT INFORMATION TO THE MAIN.RB TAB
+ponzi_scheme = Stock_Ledger.new
+
+ponzi_scheme.add_client Client.new("Bob", 50000, [1,2,3])
+ponzi_scheme.add_client Client.new("Mary", 50000, [11,22,33])
+ponzi_scheme.add_client Client.new("Michael", 50000, [111,222,333])
+ponzi_scheme.add_client Client.new("Jim", 50000, [1111,2222,3333])
+
+sally = Client.new("Sally", 50000, [11111,222222,333333])
+puts "A client arrives."
+puts sally
+
+puts "\nShe wants to see the client list."
+ponzi_scheme.list_clients
